d423d97861607d2474efe8c08ddbb311cf1044e2
maksymtymchenko <makstimchenk@gmail.com>
2025-03-05 19:09:36 +0200

    fix styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main style={{width: 1200, margin: 40}}>
         <h1>Cloud Storage</h1>
       </main>
     </>
